383c2e378e4145950a05e117ea8cb6b883f090f5
davidwajngot <david.wajngot@valtech.com>
2019-04-25 12:36:03 +0200

    dashboard image

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import Popover from '@material-ui/core/Popover';
-import Button from '@material-ui/core/Button';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Popover from "@material-ui/core/Popover";
+import Button from "@material-ui/core/Button";
 import NMISlider from "./Slider";
 import SliderGroup from "./SliderGroup";
 
@@ -15,66 +15,66 @@ const Wrapper = styled.div`
 `;
 
 const Header = styled.div`
-	background: rgb(184, 44, 112);
-	height: 2em;
-	width: 100%;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
+  background: rgb(184, 44, 112);
+  height: 2em;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Title = styled.p`
-	color: white;
-	font-size: 14px;
-	padding-left: 40px;
-	padding-right: 40px;
+  color: white;
+  font-size: 14px;
+  padding-left: 40px;
+  padding-right: 40px;
 `;
 
 const Body = styled.div`
-	width: 100%;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
 const ButtonGroup = styled.div`
-	height: 100%;
-	width: 100%;
-	display: flex;
-	justify-content: flex-end;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
 `;
 
 const ButtonDiv = styled.div`
-	padding-right: 20px;
+  padding-right: 20px;
 `;
 
 const TextButton = styled.button`
-	// width: 90px;
-	// height: 30px;
-	display-inline: block;
-	border-radius: 4px;
-	background-color: white;
-	border: 1px solid grey;
-	padding: 5px;
+  // width: 90px;
+  // height: 30px;
+  display-inline: block;
+  border-radius: 4px;
+  background-color: white;
+  border: 1px solid grey;
+  padding: 5px;
 `;
 
 const SendButton = styled.button`
-	background: ${Primary};
-	color: white;
-	display: inline-block;
-	border-radius: 4px;
-	padding: 5px;
+  background: ${Primary};
+  color: white;
+  display: inline-block;
+  border-radius: 4px;
+  padding: 5px;
 `;
 
 const Question = styled.p`
-	padding-left: 30px;
+  padding-left: 30px;
 `;
 
 const InputDiv = styled.div`
   width: 100%;
   height: 100%;
-	padding-left: 40px;
-	margin-bottom: 20px;
+  padding-left: 40px;
+  margin-bottom: 20px;
   display: flex;
   flex-direction: column;
 `;
@@ -86,93 +86,102 @@ const InputField = styled.input`
 `;
 
 const Title2 = styled.p`
-	margin-bottom: 0px;
+  margin-bottom: 0px;
 `;
 
 const PopButton = styled.button`
-	padding-right: 14px;
-	color: white;
-	background: none!important;	
-	border: none;
-	padding: 0!important;
-	&:focus {
-		outline: none;
-	}
+  padding-right: 14px;
+  color: white;
+  background: none !important;
+  border: none;
+  padding: 0 !important;
+  &:focus {
+    outline: none;
+  }
 
-	&:hover {
-		cursor: pointer;
-	}
+  &:hover {
+    cursor: pointer;
+  }
 `;
 const Img = styled.img`
-	width: 100%;
-	height: 100%;
+  width: 100%;
+  height: 100%;
 `;
 
 function Survey() {
-	const [inputOpen, setInputOpen] = useState(false);
-	const [isPopOpen, togglePopOpen] = useState(false);
-	const [isSubmitted, setSubmitted] = useState(false);
+  const [inputOpen, setInputOpen] = useState(false);
+  const [isPopOpen, togglePopOpen] = useState(false);
+  const [isSubmitted, setSubmitted] = useState(false);
 
-	let body;
+  let body;
 
-	if (!isSubmitted) {
-		body = <Body>
-			<Question>Hur tycker du att arbetsbelastningen är just nu?</Question>
-      <SliderGroup/>
-			{inputOpen &&
-				<InputDiv>
-					<Title2>Berätta mer?</Title2>
-					<InputField placeholder="Vad kan vi förändra för att det ska bli bättre?"></InputField>
-				</InputDiv>}
+  if (!isSubmitted) {
+    body = (
+      <Body>
+        <Question>Hur tycker du att arbetsbelastningen är just nu?</Question>
+        <SliderGroup />
+        {inputOpen && (
+          <InputDiv>
+            <Title2>Berätta mer?</Title2>
+            <InputField placeholder="Vad kan vi förändra för att det ska bli bättre?" />
+          </InputDiv>
+        )}
 
-			<ButtonGroup>
-				{!inputOpen &&
-					<ButtonDiv>
-						<TextButton onClick={() => setInputOpen(true)}>Berätta mer?</TextButton>
-					</ButtonDiv>}
+        <ButtonGroup>
+          {!inputOpen && (
+            <ButtonDiv>
+              <TextButton onClick={() => setInputOpen(true)}>
+                Berätta mer?
+              </TextButton>
+            </ButtonDiv>
+          )}
 
-				<ButtonDiv>
-					<SendButton onClick={() => setSubmitted(true)}>Skicka in och se resultat</SendButton>
-				</ButtonDiv>
-			</ButtonGroup>
-		</Body>;
-	} else {
-		const path = require('../images/bars.png');
-		body = <Img resizeMode="contain" src={path} />;
-	}
+          <ButtonDiv>
+            <SendButton onClick={() => setSubmitted(true)}>
+              Skicka in och se resultat
+            </SendButton>
+          </ButtonDiv>
+        </ButtonGroup>
+      </Body>
+    );
+  } else {
+    const path = require("../images/dashboard.png");
+    body = <Img resizeMode="contain" src={path} />;
+  }
 
-	return (
-		<Wrapper>
-			<Header>
-				<Title>Vi bryr oss om hur du mår</Title>
-				<PopButton onClick={() => togglePopOpen(!isPopOpen)}> Varför vill vi veta detta?</PopButton>
-				<Popover
-					open={isPopOpen}
-					onClose={() => togglePopOpen(false)}
-					anchorOrigin={{
-						vertical: 'bottom',
-						horizontal: 'center',
-					}}
-					transformOrigin={{
-						vertical: 'top',
-						horizontal: 'center',
-					}}>
-					<p>Valtech gör varje år en stor undersökning
-						för att mäta NMI (Nöjd Medarbetar Index).
-						För att bli en bättre arbetsplats vill vi
-						mäta nöjdheten oftare än så. Därför har vi
-						denna mikroundersökning för att snabbt fånga
-						upp och lösa eventuella problem. Tack för att
-						du hjälper till att göra Valtech ännu bättre!
-
-						Ps. Om du svarar ofta finns det belöningar
-						att hämta hem!
-					</p>
-				</Popover>
-			</Header>
-			{body}
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <Header>
+        <Title>Vi bryr oss om hur du mår</Title>
+        <PopButton onClick={() => togglePopOpen(!isPopOpen)}>
+          {" "}
+          Varför vill vi veta detta?
+        </PopButton>
+        <Popover
+          open={isPopOpen}
+          onClose={() => togglePopOpen(false)}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center"
+          }}
+        >
+          <p>
+            Valtech gör varje år en stor undersökning för att mäta NMI (Nöjd
+            Medarbetar Index). För att bli en bättre arbetsplats vill vi mäta
+            nöjdheten oftare än så. Därför har vi denna mikroundersökning för
+            att snabbt fånga upp och lösa eventuella problem. Tack för att du
+            hjälper till att göra Valtech ännu bättre! Ps. Om du svarar ofta
+            finns det belöningar att hämta hem!
+          </p>
+        </Popover>
+      </Header>
+      {body}
+    </Wrapper>
+  );
 }
 
 export default Survey;
