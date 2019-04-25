0145106d7125c5bdaf9a2f302ce56a46ec0cbff7
Johannes <johannes.almroth@gmail.com>
2019-04-25 10:09:22 +0200

    removed wrapper

--- src/App.js
+++ src/App.js
@@ -3,18 +3,10 @@ import './App.css';
 import Survey from './components/Survey';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  margin: 0 auto;
-`;
-
 function App() {
   return (
-    <Wrapper>
-      <Survey />
+    <Survey />
 
-    </Wrapper>
   );
 }
 
